a29a21e491baf2ad425a1adc9c4076751391e1b4
Sergei Elizarov <ser.elizarow2015@yandex.ru>
2019-02-04 19:02:51 +0300

    сделал скрипт буду делать дальше

--- js/customizer.js
+++ js/customizer.js
@@ -417,3 +417,14 @@
 		} );
 	} );
 } )( jQuery );
+
+
+//
+wp.ajax
+(function($) {var auto_refresh = setInterval(
+	$(function() {
+
+		$("#archives").load("http://domtradera.ru/wp-content/themes/newspaperly/ajax/GetNewsMain.php");
+
+	},180000))
+})(jQuery)
