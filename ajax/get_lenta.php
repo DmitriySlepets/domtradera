eaa39092b8f0ac2543822fb94ce8c607b171de59
Sergei Elizarov <ser.elizarow2015@yandex.ru>
2019-02-08 21:23:50 +0300

    реализовал условие скролла на действие пользователя

--- ajax/get_lenta.php
+++ ajax/get_lenta.php
@@ -22,18 +22,3 @@ $post=$myposts;
 ?>
 
 
-// C какой статьи будет осуществляться вывод
-$startFrom = $_POST['startFrom'];
-
-// Получаем 10 статей, начиная с последней отображенной
-$res = mysqli_query($db, "SELECT * FROM `articles` ORDER BY `article_id` DESC LIMIT {$startFrom}, 10");
-
-// Формируем массив со статьями
-$articles = array();
-while ($row = mysqli_fetch_assoc($res))
-{
-$articles[] = $row;
-}
-
-// Превращаем массив статей в json-строку для передачи через Ajax-запрос
-echo json_encode($articles);
