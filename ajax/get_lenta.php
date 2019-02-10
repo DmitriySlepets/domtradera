0b71757eb7e2c95c3a1d7810b68d9e8229d611a2
Sergei Elizarov <ser.elizarow2015@yandex.ru>
2019-02-10 21:12:15 +0300

    реализация смены получения страницы

--- ajax/get_lenta.php
+++ ajax/get_lenta.php
@@ -10,9 +10,8 @@ require_once('/var/www/u0526235/data/www/domtradera.ru/wp-load.php');
 ?>
 <?php
 global $post;
-//$startFrom = $_POST['startFrom'];
 $tmp_post = $post;
-$args = array('posts_per_page' =>10);
+$args = array('posts_per_page' =>$post);
 $myposts = get_posts($args);
 foreach ($myposts as $post):
     setup_postdata($post);
